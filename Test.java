d9509dcf02383805267fa3888121a0827663db99
Stacy Mackin <stacy.mackin@gmail.com>
2016-01-21 16:57:08 -0500

    Added count while loop to Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,12 @@
 public class Test {
 
     public static void main(String[] args) {
+    	int count = 0;
         System.out.println("Test!");
+        while(count < 10){
+        	System.out.println(count);
+        	count++;
+        }
     }
 
 }
