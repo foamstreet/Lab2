c019cec639788d68706394c99f2e7d63f56cf5c7
Stacy Mackin <stacy.mackin@gmail.com>
2016-01-21 17:01:49 -0500

    Added String variable name and for loop to print name the length of name to Test.java

--- Test.java
+++ Test.java
@@ -2,10 +2,14 @@ public class Test {
 
     public static void main(String[] args) {
     	int count = 0;
+    	String name = "Stacy";
         System.out.println("Test!");
         while(count < 10){
         	System.out.println(count);
         	count++;
+        }
+        for(int i = 0; i < name.length(); i++){
+        	System.out.println(name);
         }
     }
 
